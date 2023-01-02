658f3b2fcac5468b80d3181ce9f42fdc0826ce01
eno-conan <eno49study@gmail.com>
2023-01-02 10:36:34 +0900

    3画面遷移のルーティング実装を追加してみた

--- dropdown_sample/README.md
+++ dropdown_sample/README.md
@@ -6,6 +6,7 @@ https://blog.flutteruniv.com/flutter-dropdownbutton/
 - [23/01/02：アイコン](https://blog.flutteruniv.com/widget-icon/)
 - [23/01/02：main.dart以外から起動する方法](https://flutter.salon/flutter/not_main_dart/)
 - [23/01/02：画面遷移実装](https://codezine.jp/article/detail/14271)
+- [23/01/02：画面遷移実装2(ルーティングをmain.dartに作成して行う)](https://terupro.net/flutter-root-navigation/)
 
 ### テスト関連（少し先だと思うが）
 - [23/01/02：テスト方法](https://flutter.salon/flutter/test-cheat-sheet/)
--- dropdown_sample/lib/pages/first_page.dart
+++ dropdown_sample/lib/pages/first_page.dart
@@ -1,18 +1,22 @@
+import 'package:dropdown_sample/pages/second_page.dart';
 import 'package:flutter/material.dart';
 
 class FirstPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text("サンプルページ")),
+        appBar: AppBar(title: Text("1stページ")),
         body: Center(
           child: TextButton(
-            child: Text("メインページへ"),
-            // （1） 前の画面に戻る
-            onPressed: () {
-              Navigator.pop(context);
-            },
-          ),
+              child: Text("Secondページへ"),
+              // （1） 前の画面に戻る
+              onPressed: () {
+                Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => SecondPage(),
+                    ));
+              }),
         ));
   }
 }
