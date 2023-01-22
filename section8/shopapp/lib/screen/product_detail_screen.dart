42623339ba7436c574cd6aca41a9f6515c82ac3f
eno-conan <eno49study@gmail.com>
2023-01-22 16:24:23 +0900

    l-197:Providerと詳細画面の役割分担を行う idから商品情報を取り出す処理をProvider側に任せる

--- section8/shopapp/lib/screen/product_detail_screen.dart
+++ section8/shopapp/lib/screen/product_detail_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:provider/provider.dart';
+import 'package:shopapp/models/product.dart';
+import 'package:shopapp/providers/products.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   // final String title;
@@ -13,10 +16,14 @@ class ProductDetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     // 商品一覧からのルーティング時にargumentsが設定した値を受け取る
     final productId =
-        ModalRoute.of(context)!.settings.arguments as String; // is the id!
+        ModalRoute.of(context)!.settings.arguments as String; // is the id!]
+    // l-197：フィルタリング処理は、Provider側に任せる
+    //listen:falseに設定することで、都度都度再構築しない
+    final loadedProduct =
+        Provider.of<Products>(context, listen: false).findById(productId);
     return Scaffold(
       appBar: AppBar(
-        title: Text('title'),
+        title: Text(loadedProduct.title),
       ),
       body: Container(),
     );
